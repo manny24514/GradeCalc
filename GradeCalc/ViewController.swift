6e5dfbc71bb72bb0316e736d6db0d2c2eb534a09
MANUEL HERNANDEZ <rl106926@rlas-macair-36.local>
2018-11-12 11:15:13 -0600

    Initial Commit

---
+++ GradeCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradeCalc
+//
+//  Created by MANUEL HERNANDEZ on 11/12/18.
+//  Copyright © 2018 MANUEL HERNANDEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
